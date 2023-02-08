ce6748da91a460cacbdbbf6613e8d298f5bccc96
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-02-08 23:36:01 +0000

    Made HelloWorldStyle in Swift.

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,18 @@
+//
+
+//  HelloWorld.swift
+
+//
+//  Created by Sarah Andrew
+
+//  Created on 2023-02-07
+
+//  Version 1.0
+
+//  Copyright (c) Year Sarah. All rights reserved.
+
+//
+
+//  This program executes future plans.
+print("In the future, I'd like to go into engineering!")
+print("I'm excited to see how it'll play out.")
